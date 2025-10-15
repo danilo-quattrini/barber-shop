a78bc3efbe218a9102773ab2e903f01250059b18
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2025-10-15 22:06:13 +0200

    feat: new example homepage.

---
+++ resources/views/components/mobile-nav-links.blade.php
@@ -0,0 +1,17 @@
+<div id="mobile-navLinks" class="fixed inset-0 z-[100] bg-white/60 text-slate-800 backdrop-blur flex flex-col items-center justify-center text-lg gap-8 md:hidden transition-transform duration-300 -translate-x-full">
+    <a href="/">
+        Home
+    </a>
+    <a href="/appointments">
+        Appointments
+    </a>
+    <a href="/pricing">
+        Pricing
+    </a>
+    <a href="/contacts">
+        Contacts
+    </a>
+    <button id="close-menu" class="active:ring-3 active:ring-white aspect-square size-10 p-1 items-center justify-center bg-slate-100 hover:bg-slate-200 transition text-black rounded-md flex">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+    </button>
+</div>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<x-layout>
+    <section class="flex flex-col items-center text-sm bg-[url('https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/hero/bg-with-grid.png')] bg-cover bg-center bg-no-repeat">
+        <main class="flex flex-col items-center max-md:px-2">
+            <h1 class="text-center mt-32 text-5xl leading-[68px] md:text-6xl md:leading-[80px] font-semibold max-w-4xl text-slate-900">
+                The fastest way to go from idea to impact.
+            </h1>
+            <p class="text-center text-base text-slate-700 max-w-lg mt-2">
+                Our platform helps you build, test, and deliver faster — so you can focus on what matters.
+            </p>
+            <div class="flex items-center gap-4 mt-8">
+                <button class="flex items-center gap-2 bg-indigo-600 hover:bg-indigo-700 text-white active:scale-95 rounded-lg px-7 h-11">
+                    Get started
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.166 10h11.667m0 0L9.999 4.165m5.834 5.833-5.834 5.834" stroke="#fff" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </button>
+                <button class="border border-slate-600 active:scale-95 hover:bg-white/10 transition text-slate-600 rounded-lg px-8 h-11">
+                    Pricing
+                </button>
+            </div>
+        </main>
+    </section>
+</x-layout>
