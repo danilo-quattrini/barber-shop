e638f20ee87974396d79825794e7d6a44dab441d
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2025-10-15 23:04:37 +0200

    feat(buttons-components): create new button component different for each color.

---
+++ resources/views/components/button/white.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes->merge(['class' => 'class="inline-flex items-center rounded-md bg-white px-6 py-3 text-sm font-semibold text-gray-900 shadow-xs inset-ring inset-ring-gray-300 hover:bg-gray-50"']) }}>
+    {{ $slot }}
+</a>
