c2126c69e80d3d69076e2ac0f24eb843bf8d06aa
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2025-10-15 22:06:51 +0200

    feat: new route to point directly on the appointment page.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::view('/', 'home');
+
+Route::get('appointments', function (){
+    return view('appointment.index');
 });
