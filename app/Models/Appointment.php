4b1bb8ed705036e777276a88fc55bd3157c59ed2
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2025-10-16 13:15:13 +0200

    feat(relation): create relation 1-to-many from User to Appointment.

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Appointment extends Model
 {
     /** @use HasFactory<\Database\Factories\AppointmentFactory> */
     use HasFactory;
-    protected $fillable = ['date'];
+    protected $fillable = ['title','body', 'date'];
     public $timestamps = false;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id');
+    }
 }
