96c9c02341926af7cfbfdcde2d0e232bbafc1f83
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2025-10-16 13:12:57 +0200

    fix: change colors on button components.

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -8,8 +8,8 @@
     $baseClasses = 'inline-flex items-center justify-center font-sm rounded-md transition focus:outline-none focus:ring-2 focus:ring-offset-2';
     $colors = [
         'orange' => 'bg-orange-500 text-white hover:bg-orange-600 focus:ring-orange-400',
-        'gray' => 'bg-gray-200 text-gray-800 hover:bg-gray-300 focus:ring-gray-400',
-        'white' => 'bg-white text-slate-800 border border-gray-300 hover:bg-gray-100 focus:ring-gray-400',
+        'gray' => 'bg-gray-200 text-slate-800 hover:bg-slate-500 focus:ring-slate-400',
+        'white' => 'bg-white text-slate-700 border border-slate-500 hover:bg-slate-100 focus:ring-slate-400',
     ];
     $classes = $baseClasses . ' ' . ($colors[$color] ?? $colors['orange']) . ' px-4 py-2';
 @endphp
--- resources/views/sign-up.blade.php
+++ resources/views/sign-up.blade.php
@@ -9,11 +9,11 @@
 <body>
     <div class="flex items-center min-h-screen bg-white">
         <div class="container mx-auto">
-            <div class="max-w-xl mx-auto border rounded-md border-zinc-500">
+            <div class="max-w-xl mx-auto border rounded-md border-slate-500">
                 <div class="p-9">
                     <div class="text-center">
                         <h1 class="my-3 text-3xl font-semibold text-slate-700">Sign Up</h1>
-                        <p class="text-gray-500">Sign up to take part of our family</p>
+                        <p class="text-slate-500">Sign up to take part of our family</p>
                     </div>
                     <div class="m-7">
 
@@ -21,21 +21,21 @@
                             @csrf
                             @method('PATCH')
                             <div class="mb-6">
-                                <label for="name" class="block mb-2 text-sm text-gray-600 ">Name</label>
-                                <input type="text" name="name" id="name" placeholder="Jhon" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-orange-100 focus:border-orange-300" required/>
+                                <label for="name" class="block mb-2 text-sm text-slate-600 ">Name</label>
+                                <input type="text" name="name" id="name" placeholder="Jhon" class="w-full px-3 py-2 placeholder-slate-300 border border-slate-500 rounded-md focus:outline-none focus:ring focus:ring-orange-100 focus:border-orange-300" required/>
                             </div>
 
                             <div class="mb-6">
-                                <label for="email" class="block mb-2 text-sm text-gray-600 ">Email Address</label>
-                                <input type="email" name="email" id="email" placeholder="you@company.com" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-orange-100 focus:border-orange-300" required/>
+                                <label for="email" class="block mb-2 text-sm text-slate-600 ">Email Address</label>
+                                <input type="email" name="email" id="email" placeholder="you@company.com" class="w-full px-3 py-2 placeholder-slate-300 border border-slate-500 rounded-md focus:outline-none focus:ring focus:ring-orange-100 focus:border-orange-300" required/>
                             </div>
 
                             <div class="mb-6">
                                 <div class="flex justify-between mb-2">
-                                    <label for="password" class="text-sm text-gray-600">Password</label>
-                                    <a href="/" class="text-sm text-gray-400 focus:outline-none focus:text-orange-500 hover:text-orange-500">Forgot password?</a>
+                                    <label for="password" class="text-sm text-slate-600">Password</label>
+                                    <a href="/" class="text-sm text-slate-400 focus:outline-none focus:text-orange-500 hover:text-orange-500">Forgot password?</a>
                                 </div>
-                                <input type="password" name="password" id="password" placeholder="Your Password" class="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-orange-100 focus:border-orange-300" required/>
+                                <input type="password" name="password" id="password" placeholder="Your Password" class="w-full px-3 py-2 placeholder-slate-300 border border-slate-500 rounded-md focus:outline-none focus:ring focus:ring-orange-100 focus:border-orange-300" required/>
                             </div>
 
                             {{--Error handling div--}}
@@ -47,7 +47,7 @@
                             <div class="flex items-center justify-center mb-6">
                                 <x-button type="submit"> Sign Up </x-button>
                             </div>
-                            <p class="text-sm text-center text-gray-400">Already your customer? <a href="/" class="text-orange-400 focus:outline-none focus:underline focus:text-orange-500">Sign in</a>.</p>
+                            <p class="text-sm text-center text-slate-400">Already your customer? <a href="/" class="text-orange-400 focus:outline-none focus:underline focus:text-orange-500">Sign in</a>.</p>
                         </form>
                     </div>
                 </div>
