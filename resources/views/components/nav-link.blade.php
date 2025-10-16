0adb6293974dd0a1ba13f3e26d0ab3d907a836c0
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2025-10-16 11:00:54 +0200

    refactor: change button components inside the nav-link page.

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -18,12 +18,12 @@
     </div>
 
     <div class="hidden md:block space-x-3">
-        <x-button.orange href="/sign-up">
+        <x-button href="/sign-up">
             Get started
-        </x-button.orange>
-        <x-button.white>
+        </x-button>
+        <x-button color='white'>
             Login
-        </x-button.white>
+        </x-button>
     </div>
     <button id="open-menu" class="md:hidden active:scale-90 transition">
         <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu-icon lucide-menu"><path d="M4 5h16"/><path d="M4 12h16"/><path d="M4 19h16"/></svg>
