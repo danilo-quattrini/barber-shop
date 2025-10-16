441b36727f560e9b17445839c25f09efa2d5f37d
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2025-10-16 13:14:23 +0200

    fix(nav-link): change nav section.
    - edit value from get started to sign up.
    
    - edit size of the nav for different devices.

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,4 +1,4 @@
-<nav class="z-50 flex items-center justify-between w-full py-4 px-8 md:px-16 lg:px-24 xl:px-32 backdrop-blur text-slate-800 text-xl">
+<nav class="z-50 flex items-center justify-between w-full text-sm py-4 px-8 md:px-24 lg:px-32 xl:px-64 backdrop-blur text-slate-800 ">
     <a href="/">
        <img src="{{asset('barber-shop-logo.svg')}}" alt="barber shop logo"/>
     </a>
@@ -18,13 +18,15 @@
     </div>
 
     <div class="hidden md:block space-x-3">
-        <x-button href="/sign-up">
-            Get started
+        <x-button color="orange" href="/sign-up">
+            Sign Up
         </x-button>
         <x-button color='white'>
             Login
         </x-button>
     </div>
+
+    {{--TODO: Drop down menù for mobile devices to implement--}}
     <button id="open-menu" class="md:hidden active:scale-90 transition">
         <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu-icon lucide-menu"><path d="M4 5h16"/><path d="M4 12h16"/><path d="M4 19h16"/></svg>
     </button>
