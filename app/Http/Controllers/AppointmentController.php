68866e15f22839a7b1689b4ae4df745c56eda7b2
danilo.quattrini <danilo.quattrini@revelopsrl.com>
2025-10-17 13:07:37 +0200

    feat(new-route): create new route.
    - add new route from the web.php file, that point to the show view function, inside the AppointmentController.php.

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -2,16 +2,27 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\Appointment;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class AppointmentController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        $appointments = Appointment::all();
+        $appointments = Appointment::with('user')
+        ->latest('appointment_date')
+        ->paginate('3');
         return view('appointment.index', [
             'appointments' => $appointments
+        ]);
+    }
+
+    public function show(Appointment $appointment): View
+    {
+        return view('appointment.show', [
+            'appointment' => $appointment
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\UserController;
+use App\Models\Appointment;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
@@ -10,4 +11,8 @@ Route::get('sign-up', [UserController::class, 'index']);
 
 Route::patch('sing-up', [UserController::class, 'create']);
 
-Route::get('appointments',[AppointmentController::class, 'index']);
+Route::get('appointments',[AppointmentController::class, 'index'])
+    ->name('appointment');
+
+Route::get('appointments/{appointment}',[AppointmentController::class, 'show'])
+    ->name('appointment');
